4bfd22df346d1cffa5e00fde9d2afa5847255f55
marparc <122086810+marparc@users.noreply.github.com>
2025-11-25 14:13:00 +0800

    Exercise 1 : Adding of Assets and Db
    Added photos of books and authors
    Added data.js

---
+++ exercise-01-react-library/submissions/partosa_marc/src/db/data.js
@@ -0,0 +1,120 @@
+
+const Books = [
+    {
+        id: "BK-1",
+        title: "Harry Potter and the Philosopher's Stone",
+        category: "Fantasy",
+        authorId: "AU-1",
+        image: "../assets/images/books/Harry-Potter-and-the-Philosopher's-Stone.jpg",
+    },
+    {
+        id: "BK-2",
+        title: "Harry Potter and the Chamber of Secrets",
+        category: "Fantasy",
+        authorId: "AU-1",
+        image: "../assets/images/books/Harry-Potter-and the-Chamber-of-Secret.jpg",
+    },
+    {
+        id: "BK-3",
+        title: "The Lord of the Rings",
+        category: "Fantasy",
+        authorId: "AU-2",
+        image: "../assets/images/books/The-Lord-of-the-Rings.jpg",
+    },
+    {
+        id: "BK-4",
+        title: "The Hobbit",
+        category: "Fantasy",
+        authorId: "AU-2",
+        image: "../assets/images/books/The-Hobbit.jpg",
+    },
+    {
+        id: "BK-5",
+        title: "And Then There Were None",
+        category: "Mystery",
+        authorId: "AU-3",
+        image: "../assets/images/books/And-Then-There-Were-None.jpg",
+    },
+    {
+        id: "BK-6",
+        title: "Murder on the Orient Express",
+        category: "Mystery",
+        authorId: "AU-3",
+        image: "../assets/images/books/Murder-on-the Orient-Express.jpg",
+    },
+    {
+        id: "BK-7",
+        title: "A Tale of Two Cities",
+        category: "Historical Fiction",
+        authorId: "AU-4",
+        image: "../assets/images/books/A-Tale-of-Two-Cities.avif",
+    },
+    {
+        id: "BK-8",
+        title: "Great Expectations",
+        category: "Classic Literature",
+        authorId: "AU-4",
+        image: "../assets/images/books/Great-Expectations.jpg",
+    },
+    {
+        id: "BK-9",
+        title: "Alice's Adventures in Wonderland",
+        category: "Fantasy",
+        authorId: "AU-5",
+        image: "../assets/images/books/Alice's-Adventures-in-Wonderland.jpg",
+    },
+    {
+        id: "BK-10",
+        title: "Through the Looking-Glass",
+        category: "Fantasy",
+        authorId: "AU-5",
+        image: "../assets/images/books/Through-the-Looking-Glass.jpeg",
+    }
+];
+
+const Authors = [
+    {
+        id: "AU-1",
+        name: "J.K. Rowling",
+        image:"../assets/images/authors/JK-Rowling.png",
+    },
+    {
+        id: "AU-2",
+        name: "J.R.R. Tolkien",
+        image: "../assets/images/authors/JRR-Tolkien.jpg",
+    },
+    {
+        id: "AU-3",
+        name: "Agatha Christie",
+        image: "../assets/images/authors/Agatha-Christie.jpg",
+    },
+    {
+        id: "AU-4",
+        name: "Charles Dickens",
+        image: "../assets/images/authors/Charles-Dickens.jpg",
+    },
+    {
+        id: "AU-5",
+        name: "Lewis Carroll",
+        image: "../assets/images/authors/Lewis-Carroll.jpg",
+    }
+];
+
+
+const Members = [
+    {
+        id: "MB-1",
+        name: "Alice Johnson",
+        membershipDate: "2020-01-15",
+    },
+    {
+        id: "MB-2",
+        name: "Bob Smith",
+        membershipDate: "2019-03-22",
+    },
+    {
+        id: "MB-3",
+        name: "Charlie Brown",
+        membershipDate: "2021-07-30",
+    },
+]
