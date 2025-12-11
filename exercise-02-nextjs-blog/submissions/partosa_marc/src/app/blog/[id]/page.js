4c20b87677b030b9f2d2e58587ab2c1d868faafd
marparc <122086810+marparc@users.noreply.github.com>
2025-12-11 13:01:59 +0800

    Exercise 2: Updated Blog Details Page UI

--- exercise-02-nextjs-blog/submissions/partosa_marc/src/app/blog/[id]/page.js
+++ exercise-02-nextjs-blog/submissions/partosa_marc/src/app/blog/[id]/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import styles from '../../page.module.css';
+import blogStyles from './page.module.css';
 
 //lib
 import { getBlogDetailsById } from "../../../../lib/blogs";
@@ -10,21 +11,28 @@ export default async function BlogDetailsPage({ params }) {
     const blog = getBlogDetailsById(id);
   
     return (
-        <div >
-        
-          {blog.image && (
-            <Image
-              src={`/images/${blog.image}`}
-              alt={blog.title}
-              width={800}
-              height={450}
-            />
-          )}
-          <h2>{blog.title}</h2>
-       
-        <p>By: {blog.authorName}</p>
-        <p>{blog.description}</p>
-        <p>{blog.content}</p>
-      </div>
+        <div className={styles.page}>
+            <div className={styles.container}>
+                {blog.image && (
+                    <Image
+                        src={`/images/${blog.image}`}
+                        alt={blog.title}
+                        width={800}
+                        height={450}
+                        className={blogStyles.blogImage}
+                    />
+                )}
+                <h1 className={blogStyles.blogTitle}>{blog.title}</h1>
+                <p className={blogStyles.blogAuthor}>
+                    By: {blog.authorName}
+                </p>
+                <p className={blogStyles.blogDescription}>
+                    {blog.description}
+                </p>
+                <div className={blogStyles.blogContent}>
+                    {blog.content}
+                </div>
+            </div>
+        </div>
     );
-  }
+}
