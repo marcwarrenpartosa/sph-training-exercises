b56cf45edabed75afd17372968e0efe5dcf39a2d
marparc <marcwarren.partosa@sun-asterisk.com>
2025-12-11 13:03:12 +0800

    Exercise 2: Updated Blog Details Page

--- exercise-02-nextjs-blog/submissions/partosa_marc/components/blog.jsx
+++ exercise-02-nextjs-blog/submissions/partosa_marc/components/blog.jsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./blog.module.css";
 
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
 export default function Blog({ blog }) {
   console.log(blog.id);
 
@@ -20,7 +29,9 @@ export default function Blog({ blog }) {
         )}
         <h2>{blog.title}</h2>
         <p>{blog.description}</p>
-        <p style={{ fontSize: "13px", color: "#999" }}>By {blog.authorName}</p>
+        <p style={{ fontSize: "13px", color: "#999" }}>
+          {formatDate(blog.date)} • By {blog.authorName}
+        </p>
       </Link>
     </div>
   );
