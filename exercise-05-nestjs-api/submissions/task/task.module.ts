0266f11bd55a44654781c6edc62cfe5a7b753a7d
marparc <marcwarren.partosa@sun-asterisk.com>
2025-12-22 13:03:19 +0800

    Exercise 5: Removing of Files

--- exercise-05-nestjs-api/submissions/task/task.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TaskController } from './task.controller';
-import { Task } from './task';
-
-@Module({
-  controllers: [TaskController],
-  providers: [Task]
-})
-export class TaskModule {}
